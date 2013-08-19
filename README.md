1d5481e0d2071aec602f1a95fa474aa2d1c0da43
Ryan Clark <ryan.clark.j@gmail.com>
2013-08-19 10:50:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# dmu2css
+
+Reads an [NCGMP09 DescriptionOfMapUnits table](http://ncgmp09.github.io/core-content.html#descriptionofmapunits) from a .csv file and writes a [CartoCSS](http://www.mapbox.com/tilemill/docs/manual/carto/) stylesheet that can be used in [Tilemill](http://www.mapbox.com/tilemill/).
+
+## Install
+
+    npm install -g dmu2css
+    
+## Run
+
+Just `dmu2css`.
+
+You will be prompted for two pieces of information:
+1. DescriptionOfMapUnits file (CSV): Enter the path to a DescriptionOfMapUnits .csv file
+2. Output CartoCSS file: Enter the path to where you'd like it to write your CartoCSS file
---
+++ dmu2css.js
@@ -0,0 +1,34 @@
+var fs = require('fs'),
+    csv = require('csv'),
+    prompt = require('prompt'),
+    keys = [], records = [],
+    dmuLabel = 'DescriptionOfMapUnits file (CSV)',
+    cssLabel = 'Output CartoCSS file';
+
+prompt.get([dmuLabel, cssLabel], function (err, result) {
+    var carto = fs.createWriteStream(result[cssLabel]);
+    
+    csv()
+        .from.path(result[dmuLabel])
+        .on('record', function (row, index) {
+            if (index === 0) { keys = row; }
+            else {
+                var thisItem = {};
+                row.forEach(function (cell, i) {
+                    thisItem[keys[i]] = cell;
+                });
+                records.push(thisItem);
+            }
+        })
+        .on('end', function () {
+            carto.write('#mapunitpolys {\n');
+            records.forEach(function (unit) {
+                var delimiter = /[^0-9]/.exec(unit.AreaFillRGB),
+                    rgb = unit.AreaFillRGB.split(delimiter);
+                carto.write('  [MapUnit=\'' + unit.MapUnit + '\'] {');
+                carto.write(' polygon-fill: rgb(' + rgb.join(',') + '); }\n');
+            });
+            carto.write('}');
+        });
+});
+
